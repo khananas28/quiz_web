957ed69921947e8c82177320ec7d2d4f351539cf
khananas28 <82134655+khananas28@users.noreply.github.com>
2022-05-14 17:56:22 +0530

    Add files via upload

---
+++ js/Untitled-1.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int sum(int a, int b){
+    
+    int result=a+b;
+    return result;
+}
+int main(){
+    int a,b;
+    cin>>a>>b;
+    cout<<sum5(a,b)<<endl;
+}
---
+++ js/learn.js
@@ -0,0 +1,4 @@
+var React=require("react");
+var ReactDOM=require("react-dom");
+Reac\
+tDOM.render(<h1>hello world</h1>,document.getElementById("root"));
